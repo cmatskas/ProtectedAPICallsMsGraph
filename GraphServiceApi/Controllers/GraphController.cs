def0ada6e47ac14bd94f3e284d4fe4463bfa1278
Christos Matskas <christos.matskas@microsoft.com>
2020-05-13 22:18:36 +0000

    first commit

---
+++ GraphServiceApi/Controllers/GraphController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+
+namespace GraphServiceApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GraphController : ControllerBase
+    {
+        private GraphClientAuthProvider graphClientAuth;
+        private readonly GraphServiceClient graphClient;
+
+        public GraphController(GraphClientAuthProvider auth)
+        {
+            graphClientAuth = auth;
+            graphClient = new GraphServiceClient(graphClientAuth.AuthProvider);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<object>> Get()
+        {
+            IEnumerable<User> users;
+
+            try
+            {
+                users = await graphClient.Users.Request().GetAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                graphClientAuth.Token.ReplyForbiddenWithWwwAuthenticateHeader(graphClientAuth.Scopes, ex);
+                throw ex;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return users.Select(x => new
+            {
+                name = x.DisplayName,
+                upn = x.UserPrincipalName,
+                mail = x.Mail,
+                city = x.City,
+                jobTitle = x.JobTitle
+            });
+        }
+
+        [HttpGet("{upn}")]
+        public async Task<object> Get(string upn = "me")
+        {
+            User user;
+            try
+            {
+                if (upn.Equals("me"))
+                {
+                    user = await graphClient.Me.Request().GetAsync();
+                }
+                else
+                {
+                    user = await graphClient.Users[upn].Request().GetAsync();
+                }
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                graphClientAuth.Token.ReplyForbiddenWithWwwAuthenticateHeader(graphClientAuth.Scopes, ex);
+                throw ex;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return new
+            {
+                name = user.DisplayName,
+                upn = user.UserPrincipalName,
+                mail = user.Mail,
+                city = user.City,
+                jobTitle = user.JobTitle
+            };
+        }
+    }
+}
---
+++ GraphServiceApi/GraphClientAuthProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace GraphServiceApi
+{
+    public class GraphClientAuthProvider
+    {
+        public string[] Scopes { get; } = { "User.Read", "User.ReadWrite", "User.Read.All" };
+        public DelegateAuthenticationProvider AuthProvider { get; }
+        public ITokenAcquisition Token { get; }
+
+        public GraphClientAuthProvider(ITokenAcquisition token)
+        {
+            Token = token;
+            var accessToken = token.GetAccessTokenForUserAsync(Scopes).GetAwaiter().GetResult();
+            AuthProvider = new DelegateAuthenticationProvider(x => { 
+                x.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken); 
+                return Task.FromResult(0); });
+        }
+
+    }
+}
